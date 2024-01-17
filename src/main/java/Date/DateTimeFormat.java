0bb4fd9fd9f36efe954540c289c984bba4f8ae04
yekar <yekaranfil0@gmail.com>
2024-01-17 14:02:38 +0300

    v1

--- src/main/java/Date/DateTimeFormat.java
+++ src/main/java/Date/DateTimeFormat.java
@@ -1,12 +1,20 @@
 package Date;
 
+
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
+
 public class DateTimeFormat {
 
-    private static String dateFormat(){
+
+
+    public static void main(String[] args) {
+        System.out.println(new DateTimeFormat().dateFormat());
+    }
+
+    private String dateFormat(){
 
         LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of("America/New_York"));
 
@@ -14,9 +22,5 @@ public class DateTimeFormat {
 
         return localDateTime.format(formatter);
 
-    }
-
-    public static void main(String[] args) {
-        System.out.println(dateFormat());
     }
 }
